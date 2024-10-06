aeab0906061fe9e6493267f92e3d8ecdd6882d95
Vamsi Krishna D <vamsi@bundlen.com>
2024-10-06 22:32:00 +0530

    changed project name to dotnet-console

---
+++ config-docker/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var appConfig = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}.json", true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .AddCommandLine(args)
+    .Build();
+
+
+    var host = Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration((context, config) =>
+        {
+           config.Sources.Clear();
+           config.AddConfiguration(appConfig);
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddHostedService<HostedService>();        })
+        .Build();
+    
+    await host.RunAsync();
+
+
+
+
